53e5b90f986cb01dc69da58200e9f7460a444db4
Hixer05 <no@mail.com>
2024-06-20 21:41:41 +0200

    Hyprland basic setup w\Home-manager

--- home.nix
+++ home.nix
@@ -80,28 +80,110 @@
   # };
   
   
-#  wayland.windowManager.hyprland = {
-#   enable = true;
-#    xwayland.enable = true;
-#
-#    settings = {
-#      "$mainMod" = "SUPER"; 
-#      monitor = ",preferred,auto,1";
-#
-#      env = [
-#        "XDG_CURRENT_DESKTOP,Hyprland"
-#        "XDG_SESSION_TYPE,wayland"
-#        "XDG_SESSION_DESKTOP,Hyprland"
-#        "XCURSOR_SIZE,36"
-#        "QT_QPA_PLATFORM,wayland"
-#      ];
-#      input = {
-#        kb_layout = "us";
-#	kb_variant = "intl";
-#	kb_options = "ctrl:swapcaps";
-#      };
-#    };
-#  };
+  wayland.windowManager.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+    
+    settings = {
+      "$mainMod" = "SUPER"; 
+      monitor = [ "HDMI-A-1,preferred,auto,1" "Unknown-1, disable"];
+      "$terminal" = "alacritty";
+      "$menu" = "wofi --show drun";
+      
+      env = [
+        "XDG_CURRENT_DESKTOP,hyprland"
+        "XDG_SESSION_TYPE,wayland"
+        "XDG_SESSION_DESKTOP,Hyprland"
+        "XCURSOR_SIZE,36"
+        "QT_QPA_PLATFORM,wayland"
+      ];
+
+      exec-once = [
+        "kdeconnectd&"
+      ];
+      
+      # Look and feel
+      general = {
+        gaps_in = 0;
+        gaps_out = 0;
+        border_size = 2;
+        
+        "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+        "col.inactive_border" = "rgba(595959aa)";
+
+        resize_on_border = false;
+        allow_tearing = false;
+        layout = "dwindle";
+      };
+      decoration = {
+        rounding = 0;
+        active_opacity = 1.0;
+        inactive_opacity = 1.0;
+        drop_shadow = true;
+        shadow_range = 4;
+        shadow_render_power = 3;
+        "col.shadow" = "rgba(1a1a1aee)";
+        blur = {
+          enabled = true;
+          size = 3;
+          passes = 1;
+          vibrancy = 0.1696;
+        };
+      };
+      dwindle = {
+        pseudotile = true;
+        preserve_split = true;
+      };
+      master = {
+        new_is_master = true;
+      };
+      misc.force_default_wallpaper = 1;
+    input = {
+      kb_layout = "us";
+	    kb_variant = "intl";
+	    kb_options = "ctrl:swapcaps";
+      follow_mouse = 1;
+      sensitivity = 0;
+    };
+    bind = [
+      "$mainMod, Q, exec, $terminal"
+      "$mainMod, C, killactive, "
+      "$mainMod, M, exit, "
+      "$mainMod, R, exec, $menu"
+      "$mainMod, left, movefocus, l"
+      "$mainMod, right, movefocus, r"
+      "$mainMod, up, movefocus, u"
+      "$mainMod, down, movefocus, d"
+
+      "$mainMod, 1, workspace, 1"
+      "$mainMod, 2, workspace, 2"
+      "$mainMod, 3, workspace, 3"
+      "$mainMod, 5, workspace, 5"
+      "$mainMod, 6, workspace, 6"
+      "$mainMod, 7, workspace, 7"
+      "$mainMod, 8, workspace, 8"
+      "$mainMod, 9, workspace, 9"
+      "$mainMod, 0, workspace, 10"
+
+      "$mainMod SHIFT, 1, workspace, 1"
+      "$mainMod SHIFT, 2, workspace, 2"
+      "$mainMod SHIFT, 3, workspace, 3"
+      "$mainMod SHIFT, 5, workspace, 5"
+      "$mainMod SHIFT, 6, workspace, 6"
+      "$mainMod SHIFT, 7, workspace, 7"
+      "$mainMod SHIFT, 8, workspace, 8"
+      "$mainMod SHIFT, 9, workspace, 9"
+      "$mainMod SHIFT, 0, workspace, 10"
+
+    ];
+    bindm = [
+      "$mainMod, mouse:272, movewindow"
+      "$mainMod, mouse:273, resizewindow"
+    ];
+    windowrulev2 = "suppressevent maximize, class:.*";   
+    };
+  };
+ 
   # This value determines the home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new home Manager release introduces backwards
